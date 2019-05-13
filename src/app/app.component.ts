8a205885ca721d19602ecb51962ba97d1e7f2bde
mk12151 <montakasparsone@inbox.lv>
2019-05-13 15:12:25 +0300

    Major issues solved, added ngClass and native error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,30 +10,17 @@ export class AppComponent {
   private fooBarOutput: string = '';
   inputText: string = '';
 
-  ngOnInit(inputText: string) {
-    this.inputText = inputText;
-  }
-
-  public generateOutputText(value: string) {
-    //deals with error
-    if (!this.isANumber(value) && value) {
-      this.fooBarOutput = '';
-      if (value[0] !== "0" || /\D+/.test(value)) {
-        this.addInvalidClass();
-      }
-      else {
-        document.getElementById('inputText').className = "form-control"
-      }
-      return 0;
+  public generateOutputText(value: string):void {
+    if (!/^-?0/.test(value)) {
+     let outputText: string = value ? this.fooBarGenerator(value) : '';
+     this.fooBarOutput = outputText;
     }
-    document.getElementById('inputText').className = "form-control"
-    let outputText: string = value ? this.fooBarGenerator(value) : '';
-    this.fooBarOutput = outputText;
   }
   //Generates output text
   private fooBarGenerator(inputValue: string): string {
     let inputNumber: number = Number(inputValue);
     let response: string = '';
+    console.log(inputNumber);
     if (inputNumber !== 0 && inputNumber <= Number.MAX_SAFE_INTEGER) {
       let dividerNumbers: object = { 'Foo': 3, 'Bar': 5 }
       for (let key in dividerNumbers) {
@@ -44,16 +31,5 @@ export class AppComponent {
       }
     }
     return response
-  }
-  private isANumber(inputValue: string): boolean {
-    return /^-*[1-9][0-9]*$/.test(inputValue);
-  }
-
-  //Shows error message if input is invalid
-  private addInvalidClass() {
-    let className = document.getElementById('inputText').className
-    if (!className.includes("is-invalid")) {
-      document.getElementById('inputText').className += " is-invalid";
-    }
   }
 }
