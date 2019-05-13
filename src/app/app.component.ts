654d8b74ed5d6ffa792cf72d1166ed5d6f322b09
mk12151 <montakasparsone@inbox.lv>
2019-05-13 16:32:05 +0300

    Fixed some minor issues/linting issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,31 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private errorMessage: string = 'Error: invalid value provided.';
-  private fooBarOutput: string = '';
-  private verificationPattern: string = '^-?[0-9]*$';
+  private errorMessage = 'Error: invalid value provided.';
+  private fooBarOutput = '';
+  private verificationPattern = '^-?[0-9]*$';
 
-  public generateOutputText(value: string, isWrongInput: boolean):void {
-    if (!/^-?0/.test(value) && !isWrongInput) {
-     let outputText: string = value ? this.fooBarGenerator(value) : '';
-     this.fooBarOutput = outputText;
+  public generateOutputText(value: string, isWrongInput: boolean): void {
+    if (/^-?0/.test(value) || isWrongInput) {
+      this.fooBarOutput = '';
+    } else {
+      const outputText: string = value ? this.fooBarGenerator(value) : '';
+      this.fooBarOutput = outputText;
     }
   }
 
-  //Generates output text
+  // Generates output text
   private fooBarGenerator(inputValue: string): string {
-    let inputNumber: number = Number(inputValue);
-    let response: string = '';
-    console.log(inputNumber);
+    const inputNumber: number = Number(inputValue);
+    let response = '';
     if (inputNumber !== 0 && inputNumber <= Number.MAX_SAFE_INTEGER) {
-      let dividerNumbers: object = { 'Foo': 3, 'Bar': 5 }
-      for (let key in dividerNumbers) {
-        let value = dividerNumbers[key];
-        if (inputNumber % value == 0) {
+      const dividerNumbers: object = { Foo: 3, Bar: 5 };
+      for (const key of Object.keys(dividerNumbers)) {
+        const value = dividerNumbers[key];
+        if (inputNumber % value === 0) {
           response += key;
         }
       }
     }
-    return response
+    return response;
   }
 }
