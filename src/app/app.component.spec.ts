0e8bd0c6f8d73e4b9a302d4e9ac63daa65b68154
mk12151 <montakasparsone@inbox.lv>
2019-05-13 20:18:14 +0300

    Added tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        FormsModule,
       ],
     }).compileComponents();
   }));
@@ -16,16 +20,61 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fooBarForm'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fooBarForm');
-  });
-
-  it('should render title in a h1 tag', () => {
+  it('should render h2 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to fooBarForm!');
+    expect(compiled.querySelector('h2').textContent).toContain('Hello There!');
   });
+
+  it('should output value FooBar', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const value = '15'
+    const component = fixture.componentInstance;
+    component.generateOutputText(value, false);
+    fixture.detectChanges();
+    const outuput = fixture.debugElement.nativeElement;
+    expect(outuput.querySelector('input[disabled]').placeholder).toContain('FooBar');
+  });
+
+  it('should output value Foo', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const value = '30000003'
+    const component = fixture.componentInstance;
+    component.generateOutputText(value, false);
+    fixture.detectChanges();
+    const outuput = fixture.debugElement.nativeElement;
+    expect(outuput.querySelector('input[disabled]').placeholder).toContain('Foo');
+  });
+
+  it('should output value Bar', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const value = '-123455'
+    const component = fixture.componentInstance;
+    component.generateOutputText(value, false);
+    fixture.detectChanges();
+    const outuput = fixture.debugElement.nativeElement;
+    expect(outuput.querySelector('input[disabled]').placeholder).toContain('Bar');
+  });
+
+  it('should output no value', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const value = '005'
+    const component = fixture.componentInstance;
+    component.generateOutputText(value, false);
+    fixture.detectChanges();
+    const outuput = fixture.debugElement.nativeElement;
+    expect(outuput.querySelector('input[disabled]').placeholder).toBe('');
+  });
+
+  it('should output no value', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const value = '5'
+    const component = fixture.componentInstance;
+    component.generateOutputText(value, true);
+    fixture.detectChanges();
+    const outuput = fixture.debugElement.nativeElement;
+    expect(outuput.querySelector('input[disabled]').placeholder).toBe('');
+  });
+
 });
